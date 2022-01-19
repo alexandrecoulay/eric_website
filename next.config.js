fd426aadb68e91ca12a4e036e78620146bb5121d
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2022-01-19 10:22:45 +0100

    elsint version

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextTranslate = require('next-translate')
 module.exports = nextTranslate({
   swcMinify: true,
   reactStrictMode: true,
-  experimental: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-    /*concurrentFeatures: true,
-    serverComponents: true,*/
-  },
   images: {
     domains: [
       'cdn.boteric.fr',
