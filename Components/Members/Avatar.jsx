b14d32c38356d49e79bd5061cca5ea73110f81b6
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2023-03-16 22:39:08 +0100

    Canvas bug patch

--- Components/Members/Avatar.jsx
+++ Components/Members/Avatar.jsx
@@ -4,11 +4,10 @@ import { isMobile } from 'react-device-detect';
 
 import styles from "../../Style/Global.module.scss"
 import { userPath } from "../../Services/constante";
-import { SimpleColor } from "../../Services/Canvas";
 
 function Avatar({ user_id, name, disconnected = null, size = 33, onClick = undefined, pointer = undefined, border, className = "" }) {
     return (
-        <Image onClick={onClick} className={`${styles.rounded} ${pointer && styles.pointer} ${border && styles.border_color_primary} ${className}`} draggable="false" width={`${isMobile && size > 33 ? 50 : size}px`} height={`${isMobile && size > 33 ? 50 : size}px`} src={disconnected ? SimpleColor('#141418') : userPath(user_id, name, "avatars")} alt="User Avatar" objectFit="cover" />
+        <Image onClick={onClick} className={`${styles.rounded} ${pointer && styles.pointer} ${border && styles.border_color_primary} ${className}`} draggable="false" width={`${isMobile && size > 33 ? 50 : size}px`} height={`${isMobile && size > 33 ? 50 : size}px`} src={disconnected ? null : userPath(user_id, name, "avatars")} alt="User Avatar" objectFit="cover" />
     )
 }
 
