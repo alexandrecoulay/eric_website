9cf7563b3c364187e9b2f7824972ef05b6a70c85
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2023-03-16 23:03:50 +0100

    Update Seo.jsx

--- Components/Seo.jsx
+++ Components/Seo.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Head from 'next/head';
-import React from 'react';
-import Head from 'next/head';
 
 function Seo({ children, title, description, image, url }) {
 
